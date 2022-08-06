ea91e7a398e28618e618382676f34f34058ebef5
putrapantura <adsenekuala@gmail.com>
2022-08-06 07:51:31 +0700

    pasang library

---
+++ hooks/auth.js
@@ -0,0 +1,32 @@
+
+
+
+export const useeAuth = () => {
+const router = useRoter();
+
+// loading
+
+
+
+// User
+
+
+// Csrf
+
+
+
+// login
+
+
+
+
+// logout
+
+    return {
+        user,
+        csrf,
+        isloading,
+        login,
+        logout
+    }
+}
